a16cb688624e1fbd0fffa6deee36f6a844b8987e
HellTM <lisca94@gmail.com>
2024-06-11 22:53:50 +0200

    unfinished refactor

--- config/boards/arm/mango/widgets/pet_status.c
+++ config/boards/arm/mango/widgets/pet_status.c
@@ -19,12 +19,19 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
+struct wpm_status_state {
+    uint8_t wpm;
+};
+
 // SIT
 LV_IMG_DECLARE(pet_sit_0);
 LV_IMG_DECLARE(pet_sit_1);
 LV_IMG_DECLARE(pet_sit_2);
 const void *pet_sit_images[] = {
-    &pet_sit_0, &pet_sit_1, &pet_sit_2,
+    &pet_sit_0, 
+    &pet_sit_1, 
+    &pet_sit_2,
+    &pet_sit_1,
 };
 
 // SLOW TYPING
@@ -32,7 +39,10 @@ LV_IMG_DECLARE(pet_walk_0);
 LV_IMG_DECLARE(pet_walk_1);
 LV_IMG_DECLARE(pet_walk_2);
 const void *pet_walk_images[] = {
-    &pet_walk_0, &pet_walk_1, &pet_walk_2,
+    &pet_walk_0, 
+    &pet_walk_1, 
+    &pet_walk_2,
+    &pet_walk_1,
 };
 
 // FAST TYPING
@@ -40,17 +50,23 @@ LV_IMG_DECLARE(pet_run_0);
 LV_IMG_DECLARE(pet_run_1);
 LV_IMG_DECLARE(pet_run_2);
 const void *pet_run_images[] = {
-    &pet_run_0, &pet_run_1, &pet_run_2,
+    &pet_run_0, 
+    &pet_run_1, 
+    &pet_run_2,
+    &pet_run_1,
 };
 
 // JUMP
-// note that this animation requires 4 frames
+// note that this animation requires 4 different frames
 LV_IMG_DECLARE(pet_jump_0);
 LV_IMG_DECLARE(pet_jump_1);
 LV_IMG_DECLARE(pet_jump_2);
 LV_IMG_DECLARE(pet_jump_3);
 const void *pet_jump_images[] = {
-    &pet_jump_0, &pet_jump_1, &pet_jump_2, &pet_jump_3,
+    &pet_jump_0, 
+    &pet_jump_1, 
+    &pet_jump_2, 
+    &pet_jump_3,
 };
 
 // BARK
@@ -58,7 +74,10 @@ LV_IMG_DECLARE(pet_bark_0);
 LV_IMG_DECLARE(pet_bark_1);
 LV_IMG_DECLARE(pet_bark_2);
 const void *pet_bark_images[] = {
-    &pet_bark_0, &pet_bark_1, &pet_bark_2,
+    &pet_bark_0, 
+    &pet_bark_1, 
+    &pet_bark_2,
+    &pet_bark_1,
 };
 
 // DOWN
@@ -66,9 +85,19 @@ LV_IMG_DECLARE(pet_down_0);
 LV_IMG_DECLARE(pet_down_1);
 LV_IMG_DECLARE(pet_down_2);
 const void *pet_down_images[] = {
-    &pet_down_0, &pet_down_1, &pet_down_2,
+    &pet_down_0, 
+    &pet_down_1, 
+    &pet_down_2,
+    &pet_down_1,
 };
 
+// FRAME DURATION
+#define FRAME_DURATION_SIT 400
+#define FRAME_DURATION_WALK 400
+#define FRAME_DURATION_RUN 400
+#define FRAME_DURATION_DOWN 400
+#define FRAME_DURATION_BARK 400
+#define FRAME_DURATION_JUMP 400
 
 /* PET STATE */
 enum pet_wpm_state {
@@ -76,131 +105,96 @@ enum pet_wpm_state {
     sit,
     walk,
     run,
-} current_pet_wpm_state = unknown;
+};
 
 enum pet_action_state {
     no_action,
     down,
     bark,
     jump,
-} current_pet_action_state = no_action;
+};
+
+struct pet_status {
+    bool animation_locked = false;
+    pet_wpm_state wpm_state = unknown;
+    pet_wpm_state wpm_state_shown = unknown;
+    pet_action_state action_state = no_action;
+    pet_action_state action_state_shown = no_action;
+} pet_state;
 
 lv_anim_t anim;
 const void **images;
-int current_frame = 0;
-bool jump_interrupt = false;
-bool restart_animation = false;
-bool allow_frame_duration_change = false;
-int max_frame_duration = 300;
-int min_frame_duration = 100;
-int max_jump_frame_duration = 150;
-int current_frame_duration = 150;
-
 
 lv_obj_t *zmk_widget_pet_status_obj(struct zmk_widget_pet_status *widget) { return widget->obj; }
 
-void animate_images(void * var, int value) {
-    lv_obj_t *obj = (lv_obj_t *)var;
-    int frame_to_show = value;
-    current_frame = value;
 
-    // Recreate animation based on WPM
-    // This only happens on frame 0
-    if(restart_animation || (allow_frame_duration_change && current_frame == 0)) {
-        // reset bool if animation is restarted
-        restart_animation = false;
-
-        // prevent frame duration change until next cycle
-        allow_frame_duration_change = false;
-
-        // make sure there is a frame at screen
-        if (current_frame == 3 && images != pet_jump_images) {
-                frame_to_show = 1;
-        }
-        lv_img_set_src(obj, images[frame_to_show]);
-
-        // restart animation
-        struct zmk_widget_pet_status *widget;
-        SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) {
-            lv_anim_del(var, (lv_anim_exec_xcb_t) animate_images);
-            init_anim(widget);
-        }
-    } else {
-        if (current_pet_action_state == jump) {
-
-            // reset jump variable
-            if (current_frame == 3 && jump_interrupt == false && images == pet_jump_images) {
-                current_pet_action_state = no_action;
-                set_pet_action_state_based_on_modifiers();
-            }
-
-            // start jump only on frame 0
-            if (current_frame == 0) {
-                images = pet_jump_images;
-                jump_interrupt == true;
-            }
-
-            // reset interrupt value
-            if (current_frame == 2 && images == pet_jump_images) {
-                jump_interrupt == false;
-            }
-
-        } else {
-
-            // Change image set every frame.
-            if (current_pet_action_state == down) {
-                images = pet_down_images;
-            } else if (current_pet_action_state == bark) {
-                images = pet_bark_images;
-            } else if (current_pet_wpm_state == sit) {
-                images = pet_sit_images;
-            } else if (current_pet_wpm_state == walk) {
-                images = pet_walk_images;
-            } else if (current_pet_wpm_state == run) {
-                images = pet_run_images;
-            }
-
-            set_pet_action_state_based_on_modifiers();
-
-            if (current_pet_action_state == jump) {
-                restart_animation = true;
+static void set_animation(lv_obj_t *anim, struct pet_status *state) {
+    if (!state.animation_locked) {
+        switch (state.action_state) {
+        case down:
+                if (state.action_state_shown != state.action_state) {
+                    lv_animimg_set_src(anim, SRC(pet_down_images));
+                    lv_animimg_set_duration(anim, FRAME_DURATION_DOWN);
+                    lv_animimg_set_repeat_count(anim, LV_ANIM_REPEAT_INFINITE);
+                }
+            break;
+        case bark:
+                if (state.action_state_shown != state.action_state) {
+                    lv_animimg_set_src(anim, SRC(pet_bark_images));
+                    lv_animimg_set_duration(anim, FRAME_DURATION_BARK);
+                    lv_animimg_set_repeat_count(anim, LV_ANIM_REPEAT_INFINITE);
+                }
+            break;
+        case jump:
+                if (state.action_state_shown != state.action_state) {
+                    // the jump can't be interrupted
+                    state.animation_locked = true;
+                    lv_animimg_set_src(anim, SRC(pet_down_images));
+                    lv_animimg_set_duration(anim, FRAME_DURATION_JUMP);
+                    lv_animimg_set_repeat_count(anim, 1);
+                    lv_anim_set_completed_cb(anim, jump_completed_cb(state));
+                }
+            break;
+        default:
+            if (state.action_state_shown == no_action && state.wpm_state != state.wpm_state_shown) {
+                switch (state.wpm_state) {
+                    case sit:
+                            lv_animimg_set_src(anim, SRC(pet_sit_images));
+                            lv_animimg_set_duration(anim, FRAME_DURATION_SIT);
+                            lv_animimg_set_repeat_count(anim, LV_ANIM_REPEAT_INFINITE);
+                        break;
+                    case walk:
+                            lv_animimg_set_src(anim, SRC(pet_walk_images));
+                            lv_animimg_set_duration(anim, FRAME_DURATION_WALK);
+                            lv_animimg_set_repeat_count(anim, LV_ANIM_REPEAT_INFINITE);
+                        break;
+                    case run:
+                            lv_animimg_set_src(anim, SRC(pet_run_images));
+                            lv_animimg_set_duration(anim, FRAME_DURATION_RUN);
+                            lv_animimg_set_repeat_count(anim, LV_ANIM_REPEAT_INFINITE);
+                        break;
+                    default:
+                        // this shouldn't happen
+                        break;
+                }
+                break;
+                state.wpm_state_shown = state.wpm_state;
             }
         }
+        state.action_state_shown = state.action_state;
+        lv_animimg_start(anim);
     }
-
-    // Signal a frame duration change during the next frame 0
-    if (current_frame == 3) {
-        allow_frame_duration_change = true;
-    }
-
-    // This makes so the middle frame is reused as 4th frame allowing smoother animation.
-    // NOTE that the jump animation is excluded from this behaviour.
-    // More info about this in icons/pet_status.c
-    if (current_frame == 3 && images != pet_jump_images) {
-        frame_to_show = 1;
-    }
-
-    // set the image to show next
-    lv_img_set_src(obj, images[frame_to_show]);
 }
 
-void init_anim(struct zmk_widget_pet_status *widget) {
-    // Initialize the animation
-    lv_anim_init(&anim);
-    lv_anim_set_var(&anim, widget->obj);
-    lv_anim_set_exec_cb(&anim, (lv_anim_exec_xcb_t) animate_images);
-    lv_anim_set_time(&anim, current_frame_duration * 3);
-    lv_anim_set_values(&anim, 0, 3);
-    lv_anim_set_delay(&anim, 0);
-    lv_anim_set_repeat_count(&anim, LV_ANIM_REPEAT_INFINITE);
-    lv_anim_set_repeat_delay(&anim, current_frame_duration);
-    lv_anim_start(&anim);
+// unlocks animation changes after jump
+void jump_completed_cb (struct pet_state *state) {
+    state.animation_locked = false;
 }
 
 int zmk_widget_pet_status_init(struct zmk_widget_pet_status *widget, lv_obj_t *parent) {
-    widget->obj = lv_img_create(parent);
+    widget->obj = lv_animimg_create(parent);
 
-    current_pet_wpm_state = sit;
+    state.action_state = sit;
     init_anim(widget);
 
     sys_slist_append(&widgets, &widget->node);
@@ -214,13 +208,13 @@ void set_pet_action_state_based_on_modifiers() {
     // alt and shift -> bark
     if (((zmk_hid_get_explicit_mods() & MOD_LCTL) != 0) || ((zmk_hid_get_explicit_mods() & MOD_RCTL) != 0) || 
         ((zmk_hid_get_explicit_mods() & MOD_LGUI) != 0) || ((zmk_hid_get_explicit_mods() & MOD_RGUI) != 0)) {
-        current_pet_action_state = down;
+        pet_state.action_state = down;
     } else if (((zmk_hid_get_explicit_mods() & MOD_LSFT) != 0) || ((zmk_hid_get_explicit_mods() & MOD_RSFT) != 0) ||
                ((zmk_hid_get_explicit_mods() & MOD_LALT) != 0) || ((zmk_hid_get_explicit_mods() & MOD_RALT) != 0)) {
-        current_pet_action_state = bark;
+        pet_state.action_state = bark;
     } else {
-        if (current_pet_action_state != jump) {
-            current_pet_action_state = no_action;
+        if (pet_state.action_state != jump) {
+            pet_state.action_state = no_action;
         }
     }
 
@@ -233,27 +227,14 @@ int pet_wpm_event_listener(const zmk_event_t *eh) {
 
     SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) {
 
-        // Calculate current frame duration
-        current_frame_duration = (max_frame_duration - (ev->state * 3));
-
-        // Clamp the frame duration value
-        if (current_frame_duration <= min_frame_duration) {
-            current_frame_duration = min_frame_duration;
-        }
-
-        // Max jump frame duration
-        if (current_pet_action_state == jump && current_frame_duration > max_jump_frame_duration) {
-            current_frame_duration = max_jump_frame_duration;
-        }
-
         // Update pet status based on WPM.
         // Configurable in Kconfig.defconfig
         if (ev->state < CONFIG_CUSTOM_WIDGET_PET_WALK_WPM) {
-            current_pet_wpm_state = sit;
+            pet_state.wpm_state = sit;
         } else if (ev->state < CONFIG_CUSTOM_WIDGET_PET_RUN_WPM) {
-            current_pet_wpm_state = walk;
+            pet_state.wpm_state = walk;
         } else {
-            current_pet_wpm_state = run;
+            pet_state.wpm_state = run;
         }
     }
     return ZMK_EV_EVENT_BUBBLE;
@@ -270,7 +251,7 @@ int pet_keycode_event_listener(const zmk_event_t *eh) {
             case HID_USAGE_KEY_KEYBOARD_ESCAPE:
             case HID_USAGE_KEY_KEYBOARD_RETURN_ENTER:
                 if (ev->state) {
-                    current_pet_action_state = jump;
+                    pet_state.action_state = pet_action_state.jump;
                 }
                 break;
             default:
@@ -281,6 +262,14 @@ int pet_keycode_event_listener(const zmk_event_t *eh) {
     return ZMK_EV_EVENT_BUBBLE;
 }
 
+void wpm_status_update_cb(struct wpm_status_state state) {
+    struct zmk_widget_pet_status *widget;
+    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) { set_animation(widget->obj, state); }
+}
+
+ZMK_DISPLAY_WIDGET_LISTENER(widget_wpm_status, struct wpm_status_state, wpm_status_update_cb,
+                            wpm_status_get_state)
+ZMK_SUBSCRIPTION(zmk_widget_pet_status, zmk_wpm_state_changed);
 
 ZMK_LISTENER(zmk_widget_pet_status, pet_wpm_event_listener);
 ZMK_SUBSCRIPTION(zmk_widget_pet_status, zmk_wpm_state_changed);
