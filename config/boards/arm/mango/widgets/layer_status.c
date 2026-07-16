be8cbd7709527fd1b3e9737822a44d05116c211e
HellTM <lisca94@gmail.com>
2026-07-17 00:28:30 +0200

    ._.

--- config/boards/arm/mango/widgets/layer_status.c
+++ config/boards/arm/mango/widgets/layer_status.c
@@ -23,7 +23,8 @@ LV_IMG_DECLARE(layer_3);
 LV_IMG_DECLARE(layer_unknown);
 
 static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets); 
-lv_obj_t *layer_icon = void;
+lv_obj_t *layer_icon;
+bool initialized = false;
 
 struct layer_status_state {
     uint8_t index;
@@ -33,8 +34,9 @@ struct layer_status_state {
 
 static struct layer_status_state layer_status_get_state(const zmk_event_t *eh) {
     uint8_t index = zmk_keymap_highest_layer_active();
-    if (*layer_icon == void) {
+    if (initialized == false) {
         *layer_icon = (lv_obj_t *)layer_icon;
+        initialized = true;
     }
     return (struct layer_status_state){.index = index, .label = zmk_keymap_layer_name(index), .icon = *layer_icon};
 }
